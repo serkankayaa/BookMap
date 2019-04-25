743bdf40f931aed1b0fa14daddda46c23d4f63c8
serkankayaa <srknnkaya@gmail.com>
2019-04-25 10:28:16 +0300

    edited dto

--- BookStore.Dto/DtoBook.cs
+++ BookStore.Dto/DtoBook.cs
@@ -8,6 +8,8 @@ namespace BookStore.Dto
         public string NAME { get; set; }
         public string SUMMARY { get; set; }
         public Guid AUTHOR_ID_FK { get; set; }
-        public Guid PUBLISHER_ID_FK { get; set; }
+        public string AUTHOR_NAME { get; set; }
+        public Guid PUBLISHER_ID_FK { get; set; }        
+        public string PUBLISHER_NAME { get; set; }
     }
 }
