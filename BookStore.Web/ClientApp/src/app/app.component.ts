976523de21f6a17ab8197ee093acea2579459d82
Samet Alabey <alabeysamet@gmail.com>
2019-05-07 13:23:11 +0300

    ToastrOptions added.

--- BookStore.Web/ClientApp/src/app/app.component.ts
+++ BookStore.Web/ClientApp/src/app/app.component.ts
@@ -10,6 +10,13 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AppComponent {
   constructor(private toastrService: ToastrService) {
+    this.setToastrOptions();
+  }
+
+  setToastrOptions() {
     this.toastrService.toastrConfig.progressBar = true;
+    this.toastrService.toastrConfig.newestOnTop = true;
+    this.toastrService.toastrConfig.closeButton = true;
+    this.toastrService.toastrConfig.preventDuplicates = true;
   }
 }
