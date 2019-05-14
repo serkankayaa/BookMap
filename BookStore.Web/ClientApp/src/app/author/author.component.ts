5f6962c0baf324100c3de768c10fea82056c3ee4
Samet Alabey <samet.alabey@hotmail.com>
2019-05-14 10:36:41 +0300

    Validity added to Author Birthdate field.

--- BookStore.Web/ClientApp/src/app/author/author.component.ts
+++ BookStore.Web/ClientApp/src/app/author/author.component.ts
@@ -77,7 +77,7 @@ export class AuthorComponent implements OnInit {
         }
       });
   }
-  
+
   focusErrorInput() {
     const dirtyFormID = 'authorName';
     const focusForm = <HTMLFormElement>document.getElementById(dirtyFormID);
