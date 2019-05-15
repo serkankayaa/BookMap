554986767ddbd81b9b9e2bb55a452252522fdb44
SafaElmali <tsafaelmali@gmail.com>
2019-05-15 12:40:27 +0300

    update user password

---
+++ BookStore.Api/Migrations/20190515093328_BookStoreMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Api.Migrations
+{
+    public partial class BookStoreMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MODIFIED_BY",
+                table: "User_Password");
+
+            migrationBuilder.DropColumn(
+                name: "MODIFIED_DATE",
+                table: "User_Password");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MODIFIED_BY",
+                table: "User_Password",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MODIFIED_DATE",
+                table: "User_Password",
+                nullable: true);
+        }
+    }
+}
