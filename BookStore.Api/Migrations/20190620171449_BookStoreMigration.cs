226e087ff4f0b552a962cca4e30879ba4b98b9fb
SafaElmali <tsafaelmali@gmail.com>
2019-06-20 20:51:52 +0300

    Added migration folder

---
+++ BookStore.Api/Migrations/20190620171449_BookStoreMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Api.Migrations
+{
+    public partial class BookStoreMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
