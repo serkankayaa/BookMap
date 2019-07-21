f0280e9678cbf677e0e018f3e8918ed1cd687288
SafaElmali <tsafaelmali@gmail.com>
2019-07-21 11:42:02 +0300

    added table && updated  admin-author modal

--- BookStore.Web/src/app/admin/admin-author/admin-author.component.ts
+++ BookStore.Web/src/app/admin/admin-author/admin-author.component.ts
@@ -1,8 +1,13 @@
+import { DocumentService } from './../../services/document.service';
 import { AuthorService } from './../../services/author.service';
 import { Author } from './../../models/author';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { NgbDateStruct, NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { DateControl } from '../../helper/Validations';
 declare var $: any;
+
 
 @Component({
   selector: 'app-admin-author',
@@ -12,17 +17,33 @@ declare var $: any;
 export class AdminAuthorComponent implements OnInit {
   public authorForm: FormGroup;
   public author = new Author();
-  public AuthorList: Author[];
+  public authorList: Author[];
+  public formData = new FormData();
+  public dateModel: NgbDateStruct;
   public formSubmitted = false;
+  public uploadedImage;
 
-  constructor(private fb: FormBuilder, private authorService: AuthorService) { }
+  constructor(private fb: FormBuilder,
+    private authorService: AuthorService,
+    private config: NgbDatepickerConfig,
+    private toastr: ToastrService,
+    private documentService: DocumentService) {
 
+    // Customize default values of datepickers
+    config.minDate = { year: 1900, month: 1, day: 1 };
+    config.maxDate = { year: 2023, month: 12, day: 31 };
+  }
+
+  // TODO: Date Validaiton
   ngOnInit() {
     this.authorForm = this.fb.group({
       author: ['', Validators.required],
       birthDate: ['', Validators.required],
-      biography: ['', Validators.required]
-    });
+      biography: ['', Validators.required],
+      authorImage: ['']
+    }, {
+        dateValidator: DateControl('birthDate')
+      });
     this.getAllAuthors();
   }
 
@@ -32,15 +53,39 @@ export class AdminAuthorComponent implements OnInit {
 
   getAllAuthors() {
     this.authorService.getAllAuthors().subscribe(data => {
-      this.AuthorList = data;
+      this.authorList = data;
+      console.log(this.authorList);
     });
-    console.log(this.AuthorList);
+  }
+
+  uploadFile(file) {
+    this.uploadedImage = file[0];
+    this.formData.append('file', this.uploadedImage, this.uploadedImage.name);
+    this.documentService.postDocument(this.formData).subscribe(res => {
+      this.author.ImageIdFk = res.toString();
+    });
   }
 
   submitAuthor() {
     this.formSubmitted = true;
+    console.log(this.author);
+    console.log(this.authorForm);
     if (this.authorForm.invalid) {
       return;
     }
+    this.authorService.postAuthor(this.author).subscribe(res => {
+      console.log("authorService");
+      console.log(res);
+      if (res.status === 200 || res.statusText === 'OK') {
+        this.toastr.success(`Shop '${this.author.AuthorName}' successfully added!`, '', {
+          closeButton: true,
+          progressBar: true,
+          progressAnimation: 'decreasing',
+          timeOut: 4000
+        });
+        this.getAllAuthors();
+        $('#authorModal').modal('hide');
+      }
+    });
   }
 }
