21053b775839cb09cadc8ba746bde08a50397891
SafaElmali <tsafaelmali@gmail.com>
2019-07-22 20:06:29 +0300

    dashboard table & services updated

--- BookStore.Web/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ BookStore.Web/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { AuthorService } from './../../services/author.service';
 import { DashboardCount } from './../../models/dashboardCount';
 import { DashboardService } from './../../services/dashboard.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,12 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
   public dashboardData: DashboardCount;
+  public dashboardTableData;
+  public shopTableList;
+  public bookTableList;
+  public categoryTableList;
+  public publisherTableList;
+  public rowCount;
 
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
+    this.getShopTableData();
+    this.getBookTableData();
+    this.getCategoryTableData();
+    this.getPublisherTableData();
+    this.getDataCount();
+  }
+
+  getDataCount() {
     this.dashboardService.getDataCounts().subscribe(result => {
       this.dashboardData = result[0];
+    });
+  }
+
+  getShopTableData() {
+    this.dashboardService.getShopTableData().subscribe(shopData => {
+      // Explicitly tell TypeScript that we’re not interested in doing strict type checking.
+      this.shopTableList = (<any>shopData).body;
+    });
+  }
+  getBookTableData() {
+    this.dashboardService.getBookTableData().subscribe(bookData => {
+      // Explicitly tell TypeScript that we’re not interested in doing strict type checking.
+      this.bookTableList = (<any>bookData).body;
+    });
+  }
+
+  getCategoryTableData() {
+    this.dashboardService.getCategoryTableData().subscribe(categoryData => {
+      // Explicitly tell TypeScript that we’re not interested in doing strict type checking.
+      this.categoryTableList = (<any>categoryData).body;
+    });
+  }
+  getPublisherTableData() {
+    this.dashboardService.getPublisherTableData().subscribe(publisherData => {
+      // Explicitly tell TypeScript that we’re not interested in doing strict type checking.
+      this.publisherTableList = (<any>publisherData).body;
     });
   }
 }
--- BookStore.Web/src/app/services/dashboard.service.ts
+++ BookStore.Web/src/app/services/dashboard.service.ts
@@ -3,16 +3,53 @@ import { apiBaseUrl } from '../../config';
 import { HttpClient } from '@angular/common/http';
 import { headerContent } from '../header';
 import { Observable } from 'rxjs';
+import { DashboardCount } from '../models/dashboardCount';
 
 @Injectable()
 export class DashboardService {
   allDataCount;
+  recentShopTableData;
+  recentAuthorTableData;
+  recentBookTableData;
+  recentPublisherTableData;
+  recentCategoryTableData;
+
 
   constructor(private http: HttpClient) { }
 
   getDataCounts(): Observable<object> {
     this.allDataCount = this.http.get(apiBaseUrl + '/Dashboard/DataCount', { headers: headerContent, observe: 'body' });
-    
+
     return this.allDataCount;
+  }
+
+  getShopTableData(): Observable<object> {
+    this.recentShopTableData = this.http.get(apiBaseUrl + '/RecentShops', { headers: headerContent, observe: 'response' });
+
+    return this.recentShopTableData;
+  }
+
+  getAuthorTableData(): Observable<object> {
+    this.recentAuthorTableData = this.http.get(apiBaseUrl + '/RecentAuthors', { headers: headerContent, observe: 'response' });
+
+    return this.recentAuthorTableData;
+  }
+
+  getBookTableData(): Observable<object> {
+    this.recentBookTableData = this.http.get(apiBaseUrl + '/RecentBooks', { headers: headerContent, observe: 'response' });
+
+    return this.recentBookTableData;
+  }
+
+  getPublisherTableData(): Observable<object> {
+    this.recentPublisherTableData = this.http.get(apiBaseUrl + '/RecentPublishers', { headers: headerContent, observe: 'response' });
+
+    return this.recentPublisherTableData;
+  }
+
+  getCategoryTableData(): Observable<object> {
+    this.recentCategoryTableData = this.http.get(apiBaseUrl + '/RecentCategories', { headers: headerContent, observe: 'response' });
+
+    return this.recentCategoryTableData;
   }
 }
